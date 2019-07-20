f09174e251d8ecd09fb421e6fd6402197468f344
Chaitali Saini <chaitalisaini@gmail.com>
2019-07-20 12:53:32 +0530

    Add files via upload

---
+++ is-palindrome/src/main/java/Palindrome.java
@@ -0,0 +1,12 @@
+public class Palindrome {
+    public boolean check(String sentence) {
+        sentence = sentence.toLowerCase();
+        sentence = sentence.replaceAll(" ","");
+        for (int i = 0; i < sentence.length() / 2; i++) {
+            if (sentence.charAt(i) != sentence.charAt(sentence.length() - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
