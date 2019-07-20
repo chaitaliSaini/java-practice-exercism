68150d703254bcc62c4f2961e9ac7a8039a320aa
Chaitali Saini <chaitalisaini@gmail.com>
2019-07-21 01:56:30 +0530

    Add files via upload

--- is-palindrome/src/main/java/Palindrome.java
+++ is-palindrome/src/main/java/Palindrome.java
@@ -1,7 +1,7 @@
 public class Palindrome {
     public boolean check(String sentence) {
         sentence = sentence.toLowerCase();
-        sentence = sentence.replaceAll(" ","");
+        sentence = sentence.replaceAll("[^a-z0-9]", "");
         for (int i = 0; i < sentence.length() / 2; i++) {
             if (sentence.charAt(i) != sentence.charAt(sentence.length() - 1 - i)) {
                 return false;
